ed8f637736e7a6f41dd29325fdecf423d3fb99f4
Kyo young Lim <limkyouyou@gmail.com>
2022-12-08 14:56:37 +0900

    Create Operate function

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function divide(num1, num2) {
   return num1/num2;
 }
 
+function operate(currentOp, num1, num2) {
+  if (currentOp === add) {
+    return add(num1, num2);
+  } else if (currentOp === subtract) {
+    return subtract(num1, num2);
+  } else if (currentOp === multiply) {
+    return multiply(num1, num2);
+  } else if (currentOp === divide) {
+    return divide(num1, num2);
+  }
+}
+
