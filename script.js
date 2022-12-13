e5f902aadb1c4e157e1dd8bcdfd95355682eead3
Kyo young Lim <limkyouyou@gmail.com>
2022-12-13 12:23:02 +0900

    Break if condition into multiple lines

--- script.js
+++ script.js
@@ -131,7 +131,10 @@ function enterKeySwitch(activeEl) {
     if (activeEl.getAttribute('class') === 'digit btn') { // when a digit is focused, run enterkey as clicking the focused button
       const childSpan = activeEl.firstElementChild;
       storeDisplayNum(childSpan.dataset.num);
-    } else if (activeEl.getAttribute('class') === 'operator btn' || activeEl.getAttribute('class') === 'operate btn') { // when a operator/operate is focused, run enterkey as clicking the focused button
+    } else if (
+      activeEl.getAttribute('class') === 'operator btn' ||
+      activeEl.getAttribute('class') === 'operate btn'
+      ) { // when a operator/operate is focused, run enterkey as clicking the focused button
       const childSpan = activeEl.firstElementChild;
       runOperator(childSpan.dataset.op);
     } else {
